12c065fac7d031b8ef2e7352a6d3a566194b7614
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-08 20:04:43 +0200

    upd_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+const btnStart = document.querySelectorAll('button[data-start]');
+console.log(btnStart);
+const btnStop = document.querySelectorAll('button[data-stop]');
+console.log(btnStop);
+const body = document.querySelector('body');
+
+let changeBg
+btnStart.forEach(button => {
+    button.addEventListener('click', () => {
+        button.disabled = true;
+       changeBg = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor()
+        }, 1000);
+    });
+});
+btnStop.forEach(button => {
+    button.addEventListener('click', () => {
+        btnStart.forEach(button => {
+            button.disabled = false;
+        })
+        clearInterval(changeBg);
+    })
+});
+
